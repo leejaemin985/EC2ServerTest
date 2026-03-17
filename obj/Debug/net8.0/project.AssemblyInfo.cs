893f897b26e5815f5317ed8b54b5000fd1ae9782
DESKTOP-29AOU10\USER <leejaemin@illuni.com>
2026-03-17 11:14:32 +0900

    --

--- obj/Debug/net8.0/project.AssemblyInfo.cs
+++ obj/Debug/net8.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e33a1b1916205038b02c028ec35307e608d3b185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88764210a77393321f91e6a267f69702d721cc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
